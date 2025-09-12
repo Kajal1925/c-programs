68c5ff1b2ed9a28a80c56d7afea0d2ee44c4a56d
Kajal1925 <kajalkumari2622021@gmail.com>
2025-09-12 17:58:37 +0530

    Add files via upload

---
+++ ODD_EVEN.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main (){
+int num;
+printf("Enter the number:\n");
+scanf("%d",&num);
+if(num%2==0)
+{printf("The number is EVEN\n");
+}else {
+printf("The number is ODD\n");}
+getch();}
