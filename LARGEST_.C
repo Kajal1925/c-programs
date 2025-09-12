ca40f47f95164e16c39fd5094d796b19826bda98
Kajal1925 <kajalkumari2622021@gmail.com>
2025-09-12 18:14:18 +0530

    Add files via upload

---
+++ LARGEST_.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+int num_1,num_2;
+printf("Enter the numbers (A and B):\n");
+scanf("%d%d",&num_1,&num_2);
+if (num_1>num_2){
+printf("The number %d is LARGEST\n",num_1);
+}else{
+printf("The number %d is LARGEST\n",num_2);
+}getch();}
