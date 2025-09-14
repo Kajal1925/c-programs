817bccd701680dd9ff5cecf50c16bc6d1b05c896
Kajal1925 <kajalkumari2622021@gmail.com>
2025-09-14 23:02:21 +0530

    Add files via upload

---
+++ QUADROOT.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<math.h>
+void main(){
+float a,b,c,discriminant,root_1,root_2;
+clrscr();
+printf("Enter the cofficients a,b and c :\n");
+scanf("%f%f%f",&a,&b,&c);
+discriminant=b*b-4*a*c;
+printf("The discriminant is %f\n",discriminant);
+//condition for real and different roots
+if(discriminant>0){
+root_1=(-b+sqrt(discriminant))/(2*a);
+root_2=(-b-sqrt(discriminant))/(2*a);
+printf("The roots are:\n");
+printf("Root1=%f\n",root_1);
+printf("Root2=%f\n",root_2);
+}
+//condition for real and equal roots
+else if(discriminant==0){
+root_1=root_2=-b/(2*a);
+printf("The roots are:\n Root 1= Root 2 =%f\n",root_1);}
+//condition for imaginary roots
+else{
+printf("The roots are not real\n");
+}
+getch();}
